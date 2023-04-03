8c885028355504d452d4f0e8209d75d9ee10ecb0
dungnt-hust <tiendung160301@gmail.com>
2023-04-03 07:36:23 +0700

    init navbar

--- credit-card-app/src/App.js
+++ credit-card-app/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import NavBar from './components/NavBar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+    <div className="font-sora h-screen px-4 py-8 bg-gradient-to-b from-purple-900 to-purple-700 text-white">
+      <NavBar/>
     </div>
   );
 }
---
+++ credit-card-app/src/components/NavBar/Navbar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <nav className="flex items-center">
+            <div className="flex items-center">
+                <div className="text-20 font-extrabold">Dung NT</div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
